1258bfa96fdf33ff3bcc04cc50b2c017fa09c73b
John Levon <john.levon@joyent.com>
2019-01-04 15:45:09 +0000

    More cleanups, including overflow checking

--- src/arch/x86/image/multiboot2.c
+++ src/arch/x86/image/multiboot2.c
@@ -57,7 +57,7 @@ FEATURE ( FEATURE_IMAGE, "MBOOT2", DHCP_EB_FEATURE_MULTIBOOT2, 1 );
 
 #define BIB_MAX_SIZE 4096
 #define	BIB_ADDR(mb2) ((void *)&((mb2)->bib[(mb2)->bib_offset]))
-#define	BIB_SPACE(mb2, tagsize) (BIB_MAX_SIZE - (mb2)->bib_offset - (tagsize))
+#define BIB_REMAINING(mb2) (BIB_MAX_SIZE - (mb2)->bib_offset)
 
 struct mb2_image_header {
 	struct multiboot_header mb;
@@ -190,7 +190,7 @@ static int multiboot2_process_tag ( struct mb2 *mb2,
 
 		rc = multiboot2_inforeq ( mb2, offset + sizeof ( *tag ),
 					  nr_inforeqs );
-		if ( rc != 0 )
+		if ( rc )
 			return rc;
 		break;
 	}
@@ -284,7 +284,7 @@ static int multiboot2_process_tags ( struct mb2 *mb2 ) {
 		switch ( tag.type ) {
 		case MULTIBOOT_HEADER_TAG_END:
 			if (!saw_load) {
-				printf ( "%p missing ADDRESS\n", mb2->image );
+				printf ( "%p missing address\n", mb2->image );
 				return -ENOEXEC;
 			}
 			if (!saw_entry) {
@@ -303,7 +303,7 @@ static int multiboot2_process_tags ( struct mb2 *mb2 ) {
 
 		rc = multiboot2_process_tag ( mb2, &tag, offset );
 
-		if ( rc != 0 )
+		if ( rc )
 			return rc;
 
 		offset += tag.size + (MULTIBOOT_TAG_ALIGN - 1);
@@ -346,7 +346,7 @@ static int multiboot2_load ( struct mb2 *mb2 ) {
 				    EFI_SIZE_TO_PAGES ( memsz + buf_offset ),
 				    &buf_pa );
 
-	if ( efirc != 0 ) {
+	if ( efirc ) {
 		printf ( "Failed to allocate pages for kernel (%d) "
 			 "pa: 0x%llx size: 0x%zx\n", (int)efirc,
 			 buf_pa, memsz + buf_offset );
@@ -368,120 +368,142 @@ static int multiboot2_load ( struct mb2 *mb2 ) {
 	return 0;
 }
 
-static void align_tag_offset ( size_t *offset ) {
-	if ( ( *offset & 7 ) != 0 )
-		*offset = ( ( *offset + 8 ) & ~7 );
-}
-
-// FIXME: in general, check we have space
-
-static int multiboot2_add_cmdline ( struct mb2 *mb2 ) {
-	struct multiboot_tag_string *cmdline = BIB_ADDR ( mb2 );
-	size_t remaining = BIB_SPACE ( mb2, sizeof ( *cmdline ) );
-	char *buf = cmdline->string;
-	size_t len;
-
-	DBGC ( mb2->image, "MULTIBOOT2 CMDLINE at 0x%zx\n", mb2->bib_offset );
-	cmdline->type = MULTIBOOT_TAG_TYPE_CMDLINE;
-	cmdline->size = sizeof ( *cmdline );
-
-	len = ( format_uri ( mb2->image->uri, buf, remaining ) + 1 );
-	buf += len;
-	remaining -= len;
-	cmdline->size += len;
-
-	/* Copy command line to base memory buffer, if present */
-	if ( mb2->image->cmdline != NULL ) {
-		buf--;
-		cmdline->size--;
-		remaining++;
-		len = snprintf ( buf, remaining, " %s",
-		      mb2->image->cmdline ) + 1;
-		cmdline->size += len;
+static int bib_get_space ( struct mb2 *mb2, size_t size ) {
+	if ( BIB_REMAINING( mb2 ) < size ) {
+		printf ( "%p exceeded BIB_MAX_SIZE ", mb2->image );
+		return -ENOSPC;
 	}
 
-	mb2->bib_offset += cmdline->size;
-	align_tag_offset ( &mb2->bib_offset );
+	mb2->bib_offset += size;
+	return 0;
+}
+
+static void *bib_open_tag ( struct mb2 *mb2, multiboot_uint32_t type,
+			    size_t size ) {
+	struct multiboot_tag *tag;
+
+	if ( ( mb2->bib_offset & 7 ) != 0 ) {
+		size_t aligned_off = ( ( mb2->bib_offset + 8 ) & ~7 );
+		if ( aligned_off > BIB_MAX_SIZE )
+			return NULL;
+		mb2->bib_offset = aligned_off;
+	}
+
+	DBGC ( mb2->image, "MULTIBOOT2 tag %d at 0x%zx\n", type,
+	       mb2->bib_offset );
+
+	tag = BIB_ADDR ( mb2 );
+
+	if ( bib_get_space ( mb2, size ) )
+		return NULL;
+
+	tag->type = type;
+	return tag;
+}
+
+static void bib_close_tag ( struct mb2 *mb2, void *tagp ) {
+	struct multiboot_tag *tag = tagp;
+	tag->size = (char *)BIB_ADDR ( mb2 ) - (char *)tagp;
+}
+
+/*
+ * An image cmdline buffer is the URI of the image appended with its cmdline.
+ */
+static int multiboot2_fmt_cmdline ( struct mb2 *mb2, struct image *image,
+				    char *buf ) {
+	size_t remaining = BIB_REMAINING ( mb2 );
+	size_t len;
+
+	len = format_uri ( image->uri, buf, remaining ) + 1;
+	if ( len > remaining )
+		return -ENOSPC;
+
+	if ( image->cmdline == NULL ) {
+		mb2->bib_offset += len;
+		return 0;
+	}
+
+	/* Overwrite the NIL. */
+	len--;
+
+	mb2->bib_offset += len;
+	buf += len;
+	remaining -= len;
+
+	len = snprintf ( buf, remaining, " %s", image->cmdline ) + 1;
+	if ( len > remaining )
+		return -ENOSPC;
+
+	mb2->bib_offset += len;
+	return 0;
+}
+
+static int multiboot2_add_cmdline ( struct mb2 *mb2 ) {
+	struct multiboot_tag_string *tag;
+	int rc;
+
+	if ( ( tag = bib_open_tag ( mb2, MULTIBOOT_TAG_TYPE_CMDLINE,
+	       sizeof ( *tag ) ) ) == NULL )
+		return -ENOSPC;
+
+	rc = multiboot2_fmt_cmdline ( mb2, mb2->image, tag->string );
+
+	if ( rc )
+		return rc;
+
+	bib_close_tag ( mb2, tag );
 	return 0;
 }
 
 static int multiboot2_add_bootloader ( struct mb2 *mb2 ) {
-	struct multiboot_tag_string *bootloader = BIB_ADDR ( mb2 );
-	size_t remaining = BIB_SPACE ( mb2, sizeof ( *bootloader ) );
-	char *buf = bootloader->string;
+	struct multiboot_tag_string *tag;
 	size_t len;
 
-	len = snprintf ( buf, remaining, "iPXE %s", product_version ) + 1;
+	if ( ( tag = bib_open_tag ( mb2, MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME,
+	       sizeof ( *tag ) ) ) == NULL )
+		return -ENOSPC;
 
-	DBGC ( mb2->image, "MULTIBOOT2 BOOT_LOADER_NAME at 0x%zx\n",
-	       mb2->bib_offset );
-	bootloader->type = MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME;
-	bootloader->size = len + sizeof ( *bootloader );
+	len = snprintf ( tag->string, BIB_REMAINING ( mb2 ), "iPXE %s",
+			 product_version ) + 1;
 
-	mb2->bib_offset += bootloader->size;
-	align_tag_offset ( &mb2->bib_offset );
+	if (len > BIB_REMAINING ( mb2 ))
+		return -ENOSPC;
+
+	mb2->bib_offset += len;
+	bib_close_tag ( mb2, tag );
 	return 0;
 }
 
 static int multiboot2_add_modules ( struct mb2 * mb2 ) {
-	struct multiboot_tag_module *module;
 	struct image *module_image;
-	size_t remaining;
-	size_t len;
-	char *buf;
 
 	/* Add each image as a multiboot module */
 	for_each_image ( module_image ) {
+		struct multiboot_tag_module *tag;
+		int rc;
 
 		/* Do not include kernel image itself as a module */
 		if ( module_image == mb2->image )
 			continue;
 
-		/*
-		 * FIXME: loader allocates in chunks, then relocates after
-		 * exiting boot services. ipxe has no support for this; we might
-		 * well hit the issue where we can't actually allocate enough
-		 * pages for our huge boot archive. This is system-dependent...
-		 */
+		if ( ( tag = bib_open_tag ( mb2, MULTIBOOT_TAG_TYPE_MODULE,
+		       sizeof ( *tag ) ) ) == NULL )
+			return -ENOSPC;
 
-		/* Add module to list */
-		DBGC ( mb2->image, "MULTIBOOT2 module %s at 0x%zx\n",
-		       module_image->name, mb2->bib_offset );
-		module = BIB_ADDR ( mb2 );
-		module->type = MULTIBOOT_TAG_TYPE_MODULE;
-		module->size = sizeof ( *module );
-		module->mod_start = user_to_phys ( module_image->data, 0 );
-		module->mod_end = user_to_phys ( module_image->data,
-						 module_image->len );
+		tag->mod_start = user_to_phys ( module_image->data, 0 );
+		tag->mod_end = user_to_phys ( module_image->data,
+					      module_image->len );
 
-		buf = module->cmdline;
-		remaining = BIB_SPACE ( mb2, sizeof ( *module ) );
+		rc = multiboot2_fmt_cmdline ( mb2, module_image, tag->cmdline );
 
-		len = ( format_uri ( module_image->uri, buf, remaining ) + 1 );
-		if ( len > remaining )
-			len = remaining;
-		buf += len;
-		remaining -= len;
-		module->size += len;
+		if ( rc )
+			return rc;
 
-		/* Copy command line to base memory buffer, if present */
-		if ( module_image->cmdline != NULL ) {
-			buf--;
-			module->size--;
-			remaining++;
-			len = snprintf ( buf, remaining, " %s",
-				module_image->cmdline ) + 1;
-			if ( len > remaining )
-				len = remaining;
-			module->size += len;
-		}
-
-		mb2->bib_offset += module->size;
-		align_tag_offset ( &mb2->bib_offset );
+		bib_close_tag ( mb2, tag );
 
 		DBGC ( mb2->image, "MULTIBOOT2 %p module %s is [%x,%x): %s\n",
-		       mb2->image, module_image->name, module->mod_start,
-		       module->mod_end, module->cmdline );
+		       mb2->image, module_image->name, tag->mod_start,
+		       tag->mod_end, tag->cmdline );
 	}
 
 	return 0;
@@ -511,7 +533,7 @@ static EFI_STATUS get_efi_mmap ( struct efi_mmap *mp ) {
 	efirc = bs->GetMemoryMap ( &size, (EFI_MEMORY_DESCRIPTOR *)efi_mmap_buf,
 				   &key, &descr_size, &descr_version );
 
-	if ( efirc != 0 ) {
+	if ( efirc ) {
 		printf ( "GetMemoryMap failed with %d\n", (int)efirc );
 		return efirc;
 	}
@@ -597,7 +619,7 @@ static ssize_t multiboot2_build_mmap ( struct image *image,
 		if ( bufsize < ( ++nr ) * sizeof ( *me ) ) {
 			printf ( "not enough space for mmap (%lx < %lx)\n",
 			    bufsize, nr * sizeof ( *me ) );
-			return -ENOMEM;
+			return -ENOSPC;
 		}
 
 		me->addr = d->PhysicalStart;
@@ -617,52 +639,48 @@ static ssize_t multiboot2_build_mmap ( struct image *image,
  * MULTIBOOT_TAG_TYPE_MMAP as well.
  */
 static int multiboot2_add_mmap ( struct mb2 *mb2 ) {
-	struct multiboot_tag_efi_mmap *emt;
-	struct multiboot_tag_mmap *mt;
+	struct multiboot_tag_efi_mmap *etag;
+	struct multiboot_tag_mmap *tag;
 	struct efi_mmap em;
 	EFI_STATUS efirc;
 	ssize_t size;
 
-	DBGC ( mb2->image, "MULTIBOOT2 EFI_MMAP at 0x%zx\n", mb2->bib_offset );
+	if ( ( etag = bib_open_tag ( mb2, MULTIBOOT_TAG_TYPE_EFI_MMAP,
+	       sizeof ( *etag ) ) ) == NULL )
+		return -ENOSPC;
 
- 	emt = BIB_ADDR ( mb2 );
-	emt->type = MULTIBOOT_TAG_TYPE_EFI_MMAP;
-	emt->descr_size = em.descr_size;
-	emt->descr_vers = em.descr_version;
-
-	mb2->bib_offset += sizeof ( *emt );
+	etag->descr_size = em.descr_size;
+	etag->descr_vers = em.descr_version;
 
 	if ( ( efirc = get_efi_mmap ( &em ) ) != 0 )
 		return -EEFI ( efirc );
 
 	size = em.nr_descrs * em.descr_size;
 
-	memcpy_user ( (userptr_t)mb2->bib, mb2->bib_offset,
+	if ( bib_get_space ( mb2, size ) )
+		return -ENOSPC;
+
+	memcpy_user ( (userptr_t)mb2->bib, mb2->bib_offset - size,
 		      (userptr_t)em.mmap_buf, 0, size );
 
-	emt->size = sizeof ( *emt ) + size;
-	mb2->bib_offset += size;
-	align_tag_offset ( &mb2->bib_offset );
+	bib_close_tag ( mb2, etag );
 
-	DBGC ( mb2->image, "MULTIBOOT2 MMAP at 0x%zx\n", mb2->bib_offset );
+	if ( ( tag = bib_open_tag ( mb2, MULTIBOOT_TAG_TYPE_MMAP,
+	       sizeof ( *tag ) ) ) == NULL )
+		return -ENOSPC;
 
-	mt = BIB_ADDR ( mb2 );
-	mt->type = MULTIBOOT_TAG_TYPE_MMAP;
-	mt->entry_size = sizeof ( struct multiboot_mmap_entry );
-	mt->entry_version = 0;
-
-	mb2->bib_offset += sizeof ( *mt );
+	tag->entry_size = sizeof ( struct multiboot_mmap_entry );
+	tag->entry_version = 0;
 
 	size = multiboot2_build_mmap ( mb2->image, &em, BIB_ADDR ( mb2 ),
-				       BIB_SPACE ( mb2, 0 ) );
+				       BIB_REMAINING ( mb2 ) );
 
         if ( size < 0 )
 		return (int)size;
 
-	mt->size = sizeof ( *mt ) + size;
-	mb2->bib_offset += size;
-	align_tag_offset ( &mb2->bib_offset );
+	(void) bib_get_space ( mb2, size );
 
+	bib_close_tag ( mb2, tag );
 	return 0;
 }
 
@@ -690,7 +708,7 @@ again:
 	if ( efirc == EFI_INVALID_PARAMETER && tries++ == 0 )
 		goto again;
 
-	if ( efirc != 0 ) {
+	if ( efirc ) {
 		DBGC ( mb2->image, "ExitBootServices() failed with %d\n",
 		       (int)efirc );
 		return -EEFI ( efirc );
@@ -709,9 +727,9 @@ void multiboot2_boot ( uint32_t *bib, uint32_t entry ) {
 }
 
 static int multiboot2_exec ( struct image *image ) {
-	struct multiboot_tag_load_base_addr *load_base_addr_tag;
-	struct multiboot_tag_efi64_ih *tag_efi64_ih;
-	struct multiboot_tag_efi64 *tag_efi64;
+	struct multiboot_tag_load_base_addr *load_tag;
+	struct multiboot_tag_efi64_ih *efi64_ih_tag;
+	struct multiboot_tag_efi64 *efi64_tag;
 	struct multiboot_tag *tag;
 	uint32_t *total_sizep;
 	int rc;
@@ -739,37 +757,31 @@ static int multiboot2_exec ( struct image *image ) {
 	/* reserved field */
 	mb2.bib_offset += sizeof ( uint32_t );
 
-	DBGC ( image, "MULTIBOOT2 LOAD_BASE_ADDR at 0x%zx\n", mb2.bib_offset );
-	load_base_addr_tag = BIB_ADDR ( &mb2 );
-	load_base_addr_tag->type = MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR;
-	load_base_addr_tag->size = sizeof ( *load_base_addr_tag );
-	load_base_addr_tag->load_base_addr = mb2.load.load_addr;
-	mb2.bib_offset += load_base_addr_tag->size;
-	align_tag_offset ( &mb2.bib_offset );
+	if ( ( load_tag = bib_open_tag ( &mb2,
+	       MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR,
+	       sizeof ( *load_tag ) ) ) == NULL )
+		return -ENOSPC;
+	load_tag->load_base_addr = mb2.load.load_addr;
+	bib_close_tag ( &mb2, load_tag );
 
-	DBGC ( image, "MULTIBOOT2 EFI64_IH at 0x%zx\n", mb2.bib_offset );
-	tag_efi64_ih = BIB_ADDR ( &mb2 );
-	tag_efi64_ih->type = MULTIBOOT_TAG_TYPE_EFI64_IH;
-	tag_efi64_ih->size = sizeof ( *tag_efi64_ih );
-	tag_efi64_ih->pointer = (multiboot_uint64_t)efi_image_handle;
-	mb2.bib_offset += tag_efi64_ih->size;
-	align_tag_offset ( &mb2.bib_offset );
+	if ( ( efi64_ih_tag = bib_open_tag ( &mb2, MULTIBOOT_TAG_TYPE_EFI64_IH,
+	       sizeof ( *efi64_ih_tag ) ) ) == NULL )
+		return -ENOSPC;
+	efi64_ih_tag->pointer = (multiboot_uint64_t)efi_image_handle;
+	bib_close_tag ( &mb2, efi64_ih_tag );
 
-	DBGC ( image, "MULTIBOOT2 EFI64 at 0x%zx\n", mb2.bib_offset );
-	tag_efi64 = BIB_ADDR ( &mb2 );
-	tag_efi64->type = MULTIBOOT_TAG_TYPE_EFI64;
-	tag_efi64->size = sizeof ( *tag_efi64 );
-	tag_efi64->pointer = (multiboot_uint64_t)efi_systab;
-	mb2.bib_offset += tag_efi64->size;
-	align_tag_offset ( &mb2.bib_offset );
+	if ( ( efi64_tag = bib_open_tag ( &mb2, MULTIBOOT_TAG_TYPE_EFI64,
+	       sizeof ( *efi64_tag ) ) ) == NULL )
+		return -ENOSPC;
+	efi64_tag->pointer = (multiboot_uint64_t)efi_systab;
+	bib_close_tag ( &mb2, efi64_tag );
 
 	if ( mb2.keep_boot_services ) {
-		DBGC ( image, "MULTIBOOT2 EFI_BS at 0x%zx\n", mb2.bib_offset );
-		tag = BIB_ADDR ( &mb2 );
-		tag->type = MULTIBOOT_TAG_TYPE_EFI_BS;
-		tag->size = sizeof ( *tag );
-		mb2.bib_offset += tag->size;
-		align_tag_offset ( &mb2.bib_offset );
+		if ( ( tag = bib_open_tag ( &mb2, MULTIBOOT_TAG_TYPE_EFI_BS,
+		       sizeof ( *tag ) ) ) == NULL )
+			return -ENOSPC;
+
+		bib_close_tag ( &mb2, tag );
 	}
 
 	if ( ( rc = multiboot2_add_mmap ( &mb2 ) ) != 0 )
@@ -784,11 +796,10 @@ static int multiboot2_exec ( struct image *image ) {
 	if ( ( rc = multiboot2_add_modules ( &mb2 ) ) != 0 )
 		return rc;
 
-	/* Terminate the tags */
-	tag = BIB_ADDR ( &mb2 );
-	tag->type = 0;
-	tag->size = sizeof ( *tag );
-	mb2.bib_offset += tag->size;
+	if ( ( tag = bib_open_tag ( &mb2, MULTIBOOT_TAG_TYPE_END,
+	       sizeof ( *tag ) ) ) == NULL )
+		return -ENOSPC;
+	bib_close_tag ( &mb2, tag );
 
 	*total_sizep = mb2.bib_offset;
 
