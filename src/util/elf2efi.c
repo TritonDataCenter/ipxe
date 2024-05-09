d67fd804b335ca969012fc5b123e6043fa6a0934
Dan McDonald <danmcd@mnx.io>
2024-05-09 16:46:59 -0400

    Revert "[efi] Treat writable sections as data sections"
    This reverts commit 678a60f61d76b6fce2d9e3b323db1892f69800d3.

--- src/util/elf2efi.c
+++ src/util/elf2efi.c
@@ -624,17 +624,7 @@ static struct pe_section * process_section ( struct elf_file *elf,
 
 	/* Fill in section characteristics and update RVA limits */
 	if ( ( shdr->sh_type == SHT_PROGBITS ) &&
-	     ( shdr->sh_flags & SHF_WRITE ) ) {
-		/* .data-type section */
-		new->hdr.Characteristics =
-			( EFI_IMAGE_SCN_CNT_INITIALIZED_DATA |
-			  EFI_IMAGE_SCN_MEM_NOT_PAGED |
-			  EFI_IMAGE_SCN_MEM_READ |
-			  EFI_IMAGE_SCN_MEM_WRITE );
-		applicable_start = &data_start;
-		applicable_end = &data_mid;
-	} else if ( ( shdr->sh_type == SHT_PROGBITS ) &&
-		    ( shdr->sh_flags & SHF_EXECINSTR ) ) {
+	     ( shdr->sh_flags & SHF_EXECINSTR ) ) {
 		/* .text-type section */
 		new->hdr.Characteristics =
 			( EFI_IMAGE_SCN_CNT_CODE |
@@ -643,6 +633,16 @@ static struct pe_section * process_section ( struct elf_file *elf,
 			  EFI_IMAGE_SCN_MEM_READ );
 		applicable_start = &code_start;
 		applicable_end = &code_end;
+	} else if ( ( shdr->sh_type == SHT_PROGBITS ) &&
+		    ( shdr->sh_flags & SHF_WRITE ) ) {
+		/* .data-type section */
+		new->hdr.Characteristics =
+			( EFI_IMAGE_SCN_CNT_INITIALIZED_DATA |
+			  EFI_IMAGE_SCN_MEM_NOT_PAGED |
+			  EFI_IMAGE_SCN_MEM_READ |
+			  EFI_IMAGE_SCN_MEM_WRITE );
+		applicable_start = &data_start;
+		applicable_end = &data_mid;
 	} else if ( shdr->sh_type == SHT_PROGBITS ) {
 		/* .rodata-type section */
 		new->hdr.Characteristics =
