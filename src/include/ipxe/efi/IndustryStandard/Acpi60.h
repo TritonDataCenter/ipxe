66ec8a98d100888fae1b105f6e236e8c88bf98cb
Dan McDonald <danmcd@mnx.io>
2024-05-09 16:22:20 -0400

    Revert "[efi] Update to current EDK2 headers"
    This reverts commit df2f23e333601e587f72c68cb7b7b116796f9d3c.

--- src/include/ipxe/efi/IndustryStandard/Acpi60.h
+++ src/include/ipxe/efi/IndustryStandard/Acpi60.h
@@ -14,16 +14,6 @@ FILE_LICENCE ( BSD2_PATENT );
 
 #include <ipxe/efi/IndustryStandard/Acpi51.h>
 
-///
-/// _PSD Revision for ACPI 6.0
-///
-#define EFI_ACPI_6_0_AML_PSD_REVISION  0
-
-///
-/// _CPC Revision for ACPI 6.0
-///
-#define EFI_ACPI_6_0_AML_CPC_REVISION  2
-
 //
 // Ensure proper structure formats
 //
